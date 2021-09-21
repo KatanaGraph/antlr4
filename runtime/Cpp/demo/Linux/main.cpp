d37d93ae8fc96e9c7d3c1b14eb7d127229b0fb33
danielmawhirter <daniel@mawhirter.com>
2021-09-21 18:42:25 +0000

    for cypher

--- runtime/Cpp/demo/Linux/main.cpp
+++ runtime/Cpp/demo/Linux/main.cpp
@@ -13,26 +13,37 @@
 #include <iostream>
 
 #include "antlr4-runtime.h"
-#include "TLexer.h"
-#include "TParser.h"
+#include "CypherLexer.h"
+#include "CypherParser.h"
+#include "visitor.h"
 
 using namespace antlrcpptest;
-using namespace antlr4;
+//using namespace antlr4;
 
-int main(int , const char **) {
-  ANTLRInputStream input(u8"🍴 = 🍐 + \"😎\";(((x * π))) * µ + ∰; a + (x * (y ? 0 : 1) + z);");
-  TLexer lexer(&input);
-  CommonTokenStream tokens(&lexer);
+int main(int argc, char **argv) {
+  //ANTLRInputStream input(u8"🍴 = 🍐 + \"😎\";(((x * π))) * µ + ∰; a + (x * (y ? 0 : 1) + z);");
+  if (argc != 2) {
+    std::cerr << "./bin <cypher query>\n";
+    exit(1);
+  }
+  antlr4::ANTLRInputStream input(argv[1]);
+
+  CypherLexer lexer(&input);
+  antlr4::CommonTokenStream tokens(&lexer);
 
   tokens.fill();
   for (auto token : tokens.getTokens()) {
     std::cout << token->toString() << std::endl;
   }
 
-  TParser parser(&tokens);
-  tree::ParseTree* tree = parser.main();
+  CypherParser parser(&tokens);
+  //antlr4::tree::ParseTree* tree = parser.oC_Cypher();
+  CypherParser::OC_CypherContext* tree = parser.oC_Cypher();
 
-  std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
+  OutputVisitor visitor;
+  tree->accept(&visitor);
+
+  std::cout << tree->toStringTree(&parser, true) << std::endl << std::endl;
 
   return 0;
 }
